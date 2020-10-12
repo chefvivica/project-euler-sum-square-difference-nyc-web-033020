6344d4fdf47af30a102d56c9deb2160db6263e67
Vivica Cong Ma <vivicacongma@gmail.com>
2020-10-12 18:48:40 +0000

    Automatically backed up by Learn

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,14 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+  def initialize
+    @sum = sum 
+    @square = square
+  end 
+  
+  def differnce 
+    
+  end 
+  
+  def sum 
+  end 
+end 
